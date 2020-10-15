37571ef03d67757a9d69d4da33709dbec9711e9d
ringodev <ringodev@protonmail.com>
2020-10-15 16:38:44 +0200

    added loop with interval passed through App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Location from './Location'
-class App extends React.Component{
+
+class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state={
-           location:{}
+        this.state = {
+            location: {}
         }
     }
 
@@ -12,8 +13,10 @@ class App extends React.Component{
         return (
             <div className="App">
                 <Location
-                    app={this}/>
-                    <p>{this.getLatitude()}</p>
+                    app={this}
+                    interval={50}
+                />
+                <p>{this.getLatitude()}</p>
                 <p>{this.getLongitude()}</p>
                 <p>{this.getTimeStamp()}</p>
             </div>
@@ -21,21 +24,24 @@ class App extends React.Component{
     }
 
     // checking if location is available
-    checkLocation(){
+    checkLocation() {
         return this.state.location.coords
     }
-    getLatitude(){
-        if (this.checkLocation()){
+
+    getLatitude() {
+        if (this.checkLocation()) {
             return this.state.location.coords.latitude
         }
     }
-    getLongitude(){
-        if (this.checkLocation()){
+
+    getLongitude() {
+        if (this.checkLocation()) {
             return this.state.location.coords.longitude
         }
     }
-    getTimeStamp(){
-        if (this.checkLocation()){
+
+    getTimeStamp() {
+        if (this.checkLocation()) {
             return new Date(this.state.location.timestamp).toISOString()
         }
     }
--- src/Location.js
+++ src/Location.js
@@ -1,31 +1,42 @@
 import React from "react";
 
-export default class Location extends React.Component{
+export default class Location extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            app:{}
+            app: {},
+            grabLocation: true,
         }
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <>
                 <button onClick={() => this.getLocation()}>Get My Location</button>
+                <button onClick={() =>  this.setState({grabLocation: false})}> Stop It !</button>
             </>
         )
 
     }
 
-    getLocation(){
+
+    async getLocation() {
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((position) => {
-                this.props.app.setState({location: position})
-            });
+            this.setState({grabLocation: true})
+            while (this.state.grabLocation) {
+                navigator.geolocation.getCurrentPosition((position) => {
+                    this.props.app.setState({location: position})
+                });
+                await sleep(this.props.interval)
+            }
         } else {
             // Todo show alert
         }
     }
 
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
