48ca3f2d2aa78c3ce33d4e6dc13e45fec495445e
Christina Kohlbacher <c.kohlbacher98@gmail.com>
2020-10-22 19:55:37 +0200

    Update App.js

--- pr_se/src/App.js
+++ pr_se/src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {User} from './User';
+//import data from'./testdaten_gps.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = { // bei states wird automatisch neu gerendert, bei member-variablen nicht
+            counter:0
+        }
+        this.state ={
+            userList: [
+                {name: "Hugo", votes: 0},
+                {name: "Max", votes: 0},
+                {name: "Hans", votes: 0},
+                {name: "Müller", votes: 0},
+            ]
+        }
+    }
+
+    doClick() {
+        this.setState({counter: this.state.counter + 1})
+    }
+
+    doVote(idx){
+        let listCopy  = this.state.userList.slice()
+        listCopy[idx].votes++
+        this.setState({userList: listCopy})
+
+    }
+
+    getData() {
+        fetch('./json/data.json')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+            .catch(err => console.error(err));
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1> {"Any JS between {}"}<br />
+                {this.props.name} <br />
+                {this.props.id}<br />s
+                {this.state.counter}<br />
+                <button onClick = {() => this.doClick()}>My Button</button>
+
+                {this.state.userList.map((user, idx) =>
+                    <User
+                        name = {user.name}
+                        votes = {user.votes}
+                        id = {idx}
+                        doVote = {(data) => this.doVote(data)}
+                    />
+                )}
+
+                {this.getData()}
+
+            </div>
+        )
+    }
 }
-
-export default App;
