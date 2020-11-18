e472ff0e9c4e64edc672da066365980e15df4b82
CHu1991 <ch_hummel@yahoo.de>
2020-11-18 20:34:02 +0100

    Kartengröße

--- react-mapbox-example/src/App.js
+++ react-mapbox-example/src/App.js
@@ -3,7 +3,6 @@ import React, { useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
 
 import fetchFakeData from "./api/fetchFakeData";
-import Popup from "./components/Popup";
 import Marker from "./components/Marker";
 import "./App.css";
 
@@ -20,72 +19,12 @@ const App = () => {
       // See style options here: https://docs.mapbox.com/api/maps/#styles
       style: "mapbox://styles/mapbox/streets-v11",
       center: [-104.9876, 39.7405],
-      zoom: 12.5
+      zoom: 2.5
     });
 
     // add navigation control (zoom buttons)
     map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
-/*
-    map.on("load", () => {
-      // add the data source for new a feature collection with no features
-      map.addSource("random-points-data", {
-        type: "geojson",
-        data: {
-          type: "FeatureCollection",
-          features: []
-        }
-      });
-      // now add the layer, and reference the data source above by name
-      map.addLayer({
-        id: "random-points-layer",
-        source: "random-points-data",
-        type: "symbol",
-        layout: {
-          // full list of icons here: https://labs.mapbox.com/maki-icons
-          "icon-image": "bakery-15", // this will put little croissants on our map
-          "icon-padding": 0,
-          "icon-allow-overlap": true
-        }
-      });
-    });
-*//*
-    map.on("moveend", async () => {
-      // get new center coordinates
-      const { lng, lat } = map.getCenter();
-      // fetch new data
-      const results = await fetchFakeData({ longitude: lng, latitude: lat });
-      // update "random-points-data" source with new data
-      // all layers that consume the "random-points-data" data source will be updated automatically
-      map.getSource("random-points-data").setData(results);
-    });
 
-    // change cursor to pointer when user hovers over a clickable feature
-    map.on("mouseenter", "random-points-layer", e => {
-      if (e.features.length) {
-        map.getCanvas().style.cursor = "pointer";
-      }
-    });
-
-    // reset cursor to default when user is no longer hovering over a clickable feature
-    map.on("mouseleave", "random-points-layer", () => {
-      map.getCanvas().style.cursor = "";
-    });
-
-    // add popup when user clicks a point
-    map.on("click", "random-points-layer", e => {
-      if (e.features.length) {
-        const feature = e.features[0];
-        // create popup node
-        const popupNode = document.createElement("div");
-        ReactDOM.render(<Popup feature={feature} />, popupNode);
-        // set popup on map
-        popUpRef.current
-          .setLngLat(feature.geometry.coordinates)
-          .setDOMContent(popupNode)
-          .addTo(map);
-      }
-    });
-*/
 
     map.on('moveend', async () => {
       // get center coordinates
@@ -97,6 +36,7 @@ const App = () => {
         const { id, geometry } = result;
         // create marker node
         const markerNode = document.createElement('div');
+
         ReactDOM.render(<Marker id={id} />, markerNode);
         // add marker to map
         new mapboxgl.Marker(markerNode)
@@ -104,9 +44,6 @@ const App = () => {
             .addTo(map);
       });
     });
-
-
-
 
     // clean up on unmount
     return () => map.remove();
