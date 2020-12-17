c7bca107ec95ffcad32b1eabbf6889a6408ffa0d
karlafx <karlafuchs98@gmail.com>
2020-12-17 21:40:22 +0100

    Update Map.js
    try to add marker (not successfully)

--- client/src/Map.js
+++ client/src/Map.js
@@ -1,5 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
+import Popup from "./components/Popup";
+import ReactDOM from "react-dom";
 
 import Marker from "./components/Marker";
 import "./App.css";
@@ -25,19 +27,81 @@ const Map = (props) => {
             zoom: 10
         });
 
+
+
+        var marker = new mapboxgl.Marker()
+            .setLngLat([48.33714606104758, 14.320349050272606])
+            .addTo(map);
+
+       /* var marker = new mapboxgl.Marker({
+            color: "#FFFFFF",
+            draggable: true
+        }).setLngLat([48.33714606104758, 14.320349050272606])
+            .addTo(map);*/
+
+       /* var geojson = [
+            {
+                type: 'Feature',
+                geometry: {
+                    type: 'Point',
+                    coordinates: [48.33714606104758, 14.320349050272606]
+                }
+            },
+        ];*/
+
+        /*const Popup = ({ feature }) => {
+            const { id, name, description } = feature.properties;
+
+            return (
+                <div id={`popup-${id}`}>
+                    <h3>{name}</h3>
+                    {description}
+                </div>
+            );
+        };*/
+
         // add navigation control (zoom buttons)
         map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
+
+
 
         // creates a Marker at the Location of the passed in location object
         new mapboxgl.Marker(<Marker />)
             .setLngLat({lng:props.location.coords.longitude,lat:props.location.coords.latitude})
             .addTo(map);
 
+        /*new mapboxgl.Marker(<Marker />)
+            .setLngLat({lng:48.33714606104758, lat:14.320349050272606})
+            .addTo(map);*/
+
+
+
+        // add popup when user clicks a point
+       /* map.on("click", "random-points-layer", e => {
+            if (e.features.length) {
+                const feature = e.features[0];
+                // create popup node
+                const popupNode = document.createElement("div");
+                ReactDOM.render(<Popup feature={feature} />, popupNode);
+                // set popup on map
+                popUpRef.current
+                    .setLngLat({lng:props.location.coords.longitude,lat:props.location.coords.latitude})
+                    .setDOMContent(popupNode)
+                    .addTo(map);
+            }
+        });*/
+
+
         // clean up on unmount
         return () => map.remove();
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
     return <div className="map-container" ref={mapContainerRef} />;
+
+
 };
 
+
 export default Map;
+
+
