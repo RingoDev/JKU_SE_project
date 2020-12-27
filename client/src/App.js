76593abd02e7ad7dd6e47699c060048a59dc6314
CHu1991 <ch_hummel@yahoo.de>
2020-12-27 20:03:55 +0100

    id in App

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,8 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            location: {}
+            location: {},
+            id: {}
         }
     }
 
